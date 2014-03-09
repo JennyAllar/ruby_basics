3c5f1a4cfb79db0132019fb5b258e5697a43a250
Jenny Allar <JennyAllar@gmail.com>
2014-03-09 14:10:30 -0600

    Accepting string as input and converting to integer.

--- bin/beer_on_the_wall.rb
+++ bin/beer_on_the_wall.rb
@@ -1,5 +1,6 @@
 puts "Enter a number 1 through 99."
-bottles = integer (gets.chomp)
+bottles = gets.chomp
+bottles = bottles.to_i
 
 while bottles != 0
   puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer. Take one down and pass it around, #{bottles - 1} bottles of beer on the wall."

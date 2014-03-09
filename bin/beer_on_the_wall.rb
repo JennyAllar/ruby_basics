07bcbc0df17a8d66d51fbfeb82547a02dbc83e70
Jenny Allar <JennyAllar@gmail.com>
2014-03-09 14:12:53 -0600

    Changed while statement from 0 to 1.

--- bin/beer_on_the_wall.rb
+++ bin/beer_on_the_wall.rb
@@ -2,6 +2,6 @@ puts "Enter a number 1 through 99."
 bottles = gets.chomp
 bottles = bottles.to_i
 
-while bottles != 0
+while bottles != 1
   puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer. Take one down and pass it around, #{bottles - 1} bottles of beer on the wall."
 end

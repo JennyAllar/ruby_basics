6b343acad881447a15d5325bef20fbdfdce0d74f
Jenny Allar <JennyAllar@gmail.com>
2014-03-09 13:45:29 -0600

    Prompt user for number of bottles.

---
+++ bin/beer_on_the_wall.rb
@@ -0,0 +1,7 @@
+puts "How many bottles would you like to start with?"
+bottles = gets.chomp
+bottles = (0..99)
+
+while bottles != 0
+  puts "{bottles} bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.\n"
+end

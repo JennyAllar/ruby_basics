974b4655b1795ffd39c2af40dc1271f0101ebefe
Jenny Allar <JennyAllar@gmail.com>
2014-03-09 14:43:26 -0600

    Changed verbiage for 1 bottle.

--- bin/beer_on_the_wall.rb
+++ bin/beer_on_the_wall.rb
@@ -2,7 +2,14 @@ puts "Enter a number 1 through 99."
 bottles = gets.chomp
 bottles = bottles.to_i
 
-while bottles > 1
+while bottles > 2
   puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.
 Take one down and pass it around, #{bottles -= 1} bottles of beer on the wall."
 end
+
+while bottles > 1
+  puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.
+Take one down and pass it around, #{bottles -= 1} bottle of beer on the wall."
+end
+
+

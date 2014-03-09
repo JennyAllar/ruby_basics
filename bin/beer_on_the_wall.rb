9425637ad5143585534cf65774d23b7c76216896
Jenny Allar <JennyAllar@gmail.com>
2014-03-09 14:01:35 -0600

    Removed while statement to simplfy failure.

--- bin/beer_on_the_wall.rb
+++ bin/beer_on_the_wall.rb
@@ -1,7 +1,4 @@
 puts "Enter a number 1 through 99."
 bottles = gets.chomp
 
-
-while bottles != 0
-  puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer. Take one down and pass it around, #{bottles - 1} bottles of beer on the wall."
-end
+puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer. Take one down and pass it around, #{bottles - 1} bottles of beer on the wall."

fc4b7433e17b12802594b33c58bbddf0c223dfb5
Jenny Allar <JennyAllar@gmail.com>
2014-03-09 14:03:52 -0600

    Put while statement back in for test.

--- bin/beer_on_the_wall.rb
+++ bin/beer_on_the_wall.rb
@@ -1,4 +1,6 @@
 puts "Enter a number 1 through 99."
 bottles = gets.chomp
 
-puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer. Take one down and pass it around, #{bottles - 1} bottles of beer on the wall."
+while bottles != 0
+  puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer. Take one down and pass it around, #{bottles - 1} bottles of beer on the wall."
+end

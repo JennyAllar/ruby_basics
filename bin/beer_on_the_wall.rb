fb5b4be928464e6b819c2b6c3b41ff52e706979a
Jenny Allar <JennyAllar@gmail.com>
2014-03-09 13:54:40 -0600

    Removed line break in first verse.

--- bin/beer_on_the_wall.rb
+++ bin/beer_on_the_wall.rb
@@ -3,6 +3,5 @@ bottles = gets.chomp
 bottles = (0..99)
 
 while bottles != 0
-  puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.
-Take one down and pass it around, #{bottles - 1} bottles of beer on the wall."
+  puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer. Take one down and pass it around, #{bottles - 1} bottles of beer on the wall."
 end

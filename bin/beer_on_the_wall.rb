3a824349c0a96d75667e933f19e9bbaeffc99ba3
Jenny Allar <JennyAllar@gmail.com>
2014-03-09 13:53:09 -0600

    Used string interpolation twice more.

--- bin/beer_on_the_wall.rb
+++ bin/beer_on_the_wall.rb
@@ -3,6 +3,6 @@ bottles = gets.chomp
 bottles = (0..99)
 
 while bottles != 0
-  puts "#{bottles} bottles of beer on the wall, 99 bottles of beer.
-Take one down and pass it around, 98 bottles of beer on the wall."
+  puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.
+Take one down and pass it around, #{bottles - 1} bottles of beer on the wall."
 end

06a9a0aa1a48e225d3869411311e031ced96c318
Jenny Allar <JennyAllar@gmail.com>
2014-03-09 14:05:28 -0600

    Clarified that bottles is an integer.

--- bin/beer_on_the_wall.rb
+++ bin/beer_on_the_wall.rb
@@ -1,5 +1,5 @@
 puts "Enter a number 1 through 99."
-bottles = gets.chomp
+bottles = integer gets.chomp
 
 while bottles != 0
   puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer. Take one down and pass it around, #{bottles - 1} bottles of beer on the wall."

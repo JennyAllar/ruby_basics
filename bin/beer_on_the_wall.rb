4c8c0456adc224c54c96b40ce83a24dc7071053e
Jenny Allar <JennyAllar@gmail.com>
2014-03-09 15:37:41 -0600

    Worked out while/if nest.

--- bin/beer_on_the_wall.rb
+++ bin/beer_on_the_wall.rb
@@ -2,14 +2,24 @@ puts "Enter a number 1 through 99."
 bottles = gets.chomp
 bottles = bottles.to_i
 
-while bottles > 2
-  puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.
+while bottles >= 0
+  if bottles > 2
+    puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.
 Take one down and pass it around, #{bottles -= 1} bottles of beer on the wall."
-end
 
-while bottles > 1
-  puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.
+  elsif bottles == 2
+    puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.
 Take one down and pass it around, #{bottles -= 1} bottle of beer on the wall."
-end
 
+  elsif bottles == 1
+    puts "#{bottles} bottle of beer on the wall, #{bottles} bottle of beer.
+Take one down and pass it around, no bottles of beer on the wall."
+    bottles -= 1
+  elsif bottles == 0
+    puts "No bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall."
+    bottles-= 1
+
+  end
+end
 

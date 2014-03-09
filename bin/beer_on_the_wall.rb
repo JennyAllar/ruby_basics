e685e4f5cb4615ba2db0b18db3458bda974c0e43
Jenny Allar <JennyAllar@gmail.com>
2014-03-09 14:27:57 -0600

    Realized I didn't need to be committing each time.

--- bin/beer_on_the_wall.rb
+++ bin/beer_on_the_wall.rb
@@ -3,5 +3,6 @@ bottles = gets.chomp
 bottles = bottles.to_i
 
 while bottles > 1
-  puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer. Take one down and pass it around, #{bottles -= 1} bottles of beer on the wall."
+  puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.
+Take one down and pass it around, #{bottles -= 1} bottles of beer on the wall."
 end
